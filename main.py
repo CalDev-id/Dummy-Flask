b048f3ce292c0725b43063c0f97ef20e8d3c2c6c
Heical Chandra <haical31@gmail.com>
2024-07-24 14:14:43 +0700

    update

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+

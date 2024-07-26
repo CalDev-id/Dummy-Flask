bd5929f78028b6c22f02e8f82251da8e91a976f0
Heical Chandra <haical31@gmail.com>
2024-07-26 11:23:20 +0700

    update

---
+++ loadAgain.py
@@ -0,0 +1,7 @@
+from tensorflow.keras.models import load_model
+
+# Load the original model
+model = load_model("models/cnn_air_quality_model2.h5")
+
+# Save the model again
+# model.save("models/marine_classification_resaved.h5")

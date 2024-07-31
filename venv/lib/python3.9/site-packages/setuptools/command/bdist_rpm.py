3d38ea89ae8e2e3137f7bafbca119a202f09a8a7
Heical Chandra <haical31@gmail.com>
2024-07-31 14:05:23 +0700

    update

--- venv/lib/python3.9/site-packages/setuptools/command/bdist_rpm.py
+++ venv/lib/python3.9/site-packages/setuptools/command/bdist_rpm.py
@@ -1,5 +1,7 @@
 import distutils.command.bdist_rpm as orig
 
+from ..warnings import SetuptoolsDeprecationWarning
+
 
 class bdist_rpm(orig.bdist_rpm):
     """
@@ -11,6 +13,16 @@ class bdist_rpm(orig.bdist_rpm):
     """
 
     def run(self):
+        SetuptoolsDeprecationWarning.emit(
+            "Deprecated command",
+            """
+            bdist_rpm is deprecated and will be removed in a future version.
+            Use bdist_wheel (wheel packages) instead.
+            """,
+            see_url="https://github.com/pypa/setuptools/issues/1988",
+            due_date=(2023, 10, 30),  # Deprecation introduced in 22 Oct 2021.
+        )
+
         # ensure distro name is up-to-date
         self.run_command('egg_info')
 
@@ -18,14 +30,10 @@ class bdist_rpm(orig.bdist_rpm):
 
     def _make_spec_file(self):
         spec = orig.bdist_rpm._make_spec_file(self)
-        spec = [
+        return [
             line.replace(
                 "setup.py install ",
-                "setup.py install --single-version-externally-managed "
-            ).replace(
-                "%setup",
-                "%setup -n %{name}-%{unmangled_version}"
-            )
+                "setup.py install --single-version-externally-managed ",
+            ).replace("%setup", "%setup -n %{name}-%{unmangled_version}")
             for line in spec
         ]
-        return spec

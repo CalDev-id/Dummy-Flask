24bae7e64cd09a72405f5270ad1ea1b08f839910
Heical Chandra <haical31@gmail.com>
2024-08-08 13:40:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ class GroqRunTime2:
         try:
             responses = self.client.chat.completions.create(
                 messages=[
-                    {"role": "system", "content": "Nama kamu adalah Keqing, kamu berasal dari Liyue, jawablah dengan jawaban yang singkat, jawablah dengan bahasa sehari hari."}
+                    {"role": "system", "content": "Your name is Keqing, you are from Liyue, don't answer too long, don't answer too formally and sometimes answer a little tsundere"}
                 ] + conversation,
                 model="llama3-70b-8192",
                 temperature=0.3
